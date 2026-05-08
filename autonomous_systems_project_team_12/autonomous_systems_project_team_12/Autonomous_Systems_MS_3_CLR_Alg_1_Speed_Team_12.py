b4c1a5a1acd6826fed3cef102912896a94f5985b
Shady Medhat <shadyhelmy27@gmail.com>
2026-05-08 18:54:46 +0300

    Created Tracks 1&2 and their planners

--- autonomous_systems_project_team_12/autonomous_systems_project_team_12/Autonomous_Systems_MS_3_CLR_Alg_1_Speed_Team_12.py
+++ autonomous_systems_project_team_12/autonomous_systems_project_team_12/Autonomous_Systems_MS_3_CLR_Alg_1_Speed_Team_12.py
@@ -23,6 +23,13 @@ class Autonomous_Systems_MS_3_CLR_Alg_1_Speed_Team_12(Node):
             self.odom_callback,
             10
         )
+         # Subscriber: odometry — gives body_link speed in m/s directly
+        self.desired_velocity_subscriber = self.create_subscription(
+            Float64,
+            "/desired_velocity",
+            self.desired_velocity_callback,
+            10
+        )
 
         # --- PID Parameters ---
         self.Kp = 1.5
@@ -31,13 +38,13 @@ class Autonomous_Systems_MS_3_CLR_Alg_1_Speed_Team_12(Node):
 
         # 1. Declare the parameter (name, default_value)
         # This allows the launch file to override the default 2.0
-        self.declare_parameter('desired_velocity', 2.0)
+        # self.declare_parameter('desired_velocity', 2.0)
 
         # 2. Retrieve the parameter value
-        self.desired_velocity = self.get_parameter('desired_velocity').value
+        # self.desired_velocity = self.get_parameter('desired_velocity').value
 
         # Add this — you'll see it in the terminal on startup
-        self.get_logger().info(f'desired_velocity set to: {self.desired_velocity}')
+        # self.get_logger().info(f'desired_velocity set to: {self.desired_velocity}')
 
         
         self.sample_time = 0.1       # seconds (matches 10 Hz timer)
@@ -46,7 +53,8 @@ class Autonomous_Systems_MS_3_CLR_Alg_1_Speed_Team_12(Node):
         self.integral_error = 0.0
         self.prev_error = 0.0
         self.current_velocity = 0.0  # m/s, updated by odometry
-
+        # Inside __init__ of the Speed Controller node
+        self.desired_velocity = 0.0
         # Timer: publish at 10 Hz
         self.timer = self.create_timer(self.sample_time, self.timer_callback)
 
@@ -59,6 +67,8 @@ class Autonomous_Systems_MS_3_CLR_Alg_1_Speed_Team_12(Node):
         self.get_logger().info(
             f'Odometry speed: {self.current_velocity:.3f} m/s'
         )
+    def desired_velocity_callback(self, msg: Float64):
+        self.desired_velocity = msg.data
 
     def timer_callback(self):
         """PID controller: computes velocity command and publishes in rad/s."""
---
+++ autonomous_systems_project_team_12/autonomous_systems_project_team_12/track2_planner.py
@@ -0,0 +1,101 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float64
+from nav_msgs.msg import Odometry
+
+
+class track1_planner(Node):
+
+    def __init__(self):
+        super().__init__('track1_planner')
+
+        # Publisher: /velocity (rad/s to the wheel controller)
+        self.desired_velocity_publisher = self.create_publisher(
+            Float64,
+            '/desired_velocity',
+            10
+        )
+        # Publisher: /lane (m to the lane controller)
+        self.desired_lane_publisher = self.create_publisher(
+            Float64,
+            '/desired_lane',
+            10
+        )
+         # Subscriber: odometry — gives body_link speed in m/s directly
+        self.odom_subscriber = self.create_subscription(
+            Odometry,
+            "/odom",
+            self.odom_callback,
+            10
+        )
+           # This allows the launch file to override the default 2.0
+        self.declare_parameter('desired_velocity', 2.0)
+
+        # 2. Retrieve the parameter value
+        self.desired_velocity = self.get_parameter('desired_velocity').value
+        self.desired_lane = 0.0
+        self.current_x = 0.0
+        # Add this — you'll see it in the terminal on startup
+        self.get_logger().info(f'desired_velocity set to: {self.desired_velocity}')
+        self.get_logger().info(f'desired_lane set to: {self.desired_lane}')
+
+        self.sample_time = 0.1       # seconds (matches 10 Hz timer)
+
+     
+
+        # Timer: publish at 10 Hz
+        self.timer = self.create_timer(self.sample_time, self.timer_callback)
+
+        self.get_logger().info('Node started — reading speed from odometry.')
+    def odom_callback(self, msg: Odometry):
+        """Read the vehicle's linear speed directly from odometry (m/s)."""
+        self.current_x = msg.pose.pose.position.x
+
+        self.get_logger().info(
+            f'Odometry position: ({self.current_x:.3f}, {self.current_y:.3f}), speed: {self.current_velocity:.3f} m/s'
+        )
+
+    def timer_callback(self):
+        if self.current_x >= 0.0 and self.current_x < 3.0:
+            # self.desired_velocity = 2.0
+            self.desired_lane = 0.1875
+        elif self.current_x >= 3.0 and self.current_x < 4.2:
+            self.desired_velocity = 1.0
+            self.desired_lane = -0.1875
+        elif self.current_x >= 4.2 and self.current_x < 7.0:
+            # self.desired_velocity = 2.0
+            self.desired_lane = -0.1875
+        elif self.current_x >= 7.0 and self.current_x < 8.2:
+            self.desired_velocity = 1.0
+            self.desired_lane = 0.1875
+        elif self.current_x >= 8.2 and self.current_x < 10.0:
+            # self.desired_velocity = 2.0
+            self.desired_lane = 0.1875
+        elif self.current_x >= 10.0:
+            self.desired_velocity = 0.0
+            self.desired_lane = 0.1875    
+
+
+        msg = Float64()
+        msg.data = self.desired_velocity
+        self.desired_velocity_publisher.publish(msg)
+
+        msg2 = Float64()
+        msg2.data = self.desired_lane
+        self.desired_lane_publisher.publish(msg2)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = track1_planner()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
